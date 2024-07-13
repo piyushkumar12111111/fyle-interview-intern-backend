a838979825da00482d98ecb0dd57cd137ae8eeac
@piyush100 <piyush@pilot-tech.co>
2024-07-13 14:20:25 +0530

    list all teacher working correct

---
+++ core/apis/teachers/principal.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from core.apis import decorators,responses
+from core.models.teachers import Teacher
+
+from .schema import TeacherSchema
+
+
+principal_teachers_resources = Blueprint("principal_teachers_resources",__name__)
+
+@principal_teachers_resources.route("/",methods=["GET"],strict_slashes=False)
+@decorators.authenticate_principal
+def get_all_assigments(r):
+    """Return All Teachers List"""
+    all_teachers = Teacher.get_all_teachers()
+    all_teachers_dump = TeacherSchema().dump(all_teachers, many=True)
+    return responses.APIResponse.respond(data=all_teachers_dump)
+
+    
