06dad21b2d8794e0b49dbdcdd3f677c99511a8f2
@piyush100 <piyush@pilot-tech.co>
2024-07-15 19:14:05 +0530

    test cases passed

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,19 +1,22 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
 WITH TeacherGradingCount AS (
-    SELECT teacher_id, COUNT(*) AS graded_count FROM assignments 
-    WHERE grade IS NOT NULL 
-    GROUP BY teacher_id
-),
-TopTeacher AS (
-    SELECT teacher_id FROM TeacherGradingCount
-    ORDER BY graded_count DESC
+    SELECT 
+        teacher_id, 
+        COUNT(*) AS graded_count,
+        COUNT(CASE WHEN grade = 'A' THEN 1 END) AS grade_A_count
+    FROM 
+        assignments 
+    WHERE 
+        grade IS NOT NULL 
+    GROUP BY 
+        teacher_id
+    ORDER BY 
+        graded_count DESC, 
+        grade_A_count DESC
     LIMIT 1
 )
 SELECT 
-    COUNT(*) AS grade_A_count
+    grade_A_count
 FROM 
-    assignments
-WHERE 
-    teacher_id = (SELECT teacher_id FROM TopTeacher)
-    AND grade = 'A';
+    TeacherGradingCount;
--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,2 +1,13 @@
 -- Write query to get number of graded assignments for each student:
-  SELECT student_id, COUNT(*) FROM assignments WHERE state = 'GRADED' GROUP BY student_id
+SELECT 
+    student_id, 
+    COUNT(*) AS graded_assignments_count
+FROM 
+    assignments 
+WHERE 
+    state = 'GRADED' 
+GROUP BY 
+    student_id
+ORDER BY 
+    graded_assignments_count DESC;
+
